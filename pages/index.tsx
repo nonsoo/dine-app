a60dc9631cd39055bfeaf3df1539aca33ec5b3cb
Nonso <nonsootoh@gmail.com>
2022-10-01 16:48:07 -0400

    feat: adds footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,7 +125,28 @@ const Home: NextPage = () => {
             <p className={styles.gathering_Options_optionText}>Social Event</p>
           </div>
         </section>
+
+        <section className={styles.orderingSection}>
+          <p className={styles.orderText}>Ready to make a reservation?</p>
+          <Btn btnName="book table" exCSS={styles.order_Btn} />
+        </section>
       </main>
+
+      <footer className={styles.footer}>
+        <p className={styles.footer_Title}>dine</p>
+        <div className={styles.footer_Location}>
+          <p className={styles.footer_Location_Text}>marthwaite, sedbergh</p>
+          <p className={styles.footer_Location_Text}>cumbria</p>
+          <p className={styles.footer_Location_Text}>+00 44 123 4567</p>
+        </div>
+        <div className={styles.footer_Time}>
+          <p className={styles.footer_Time_Text}>open times</p>
+          <p className={styles.footer_Time_Text}>mon - fri 09:00 am - 10 pm</p>
+          <p className={styles.footer_Time_Text}>
+            sat - sun: 09:00 am - 11:30 pm
+          </p>
+        </div>
+      </footer>
     </>
   );
 };
