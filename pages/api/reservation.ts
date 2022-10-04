c435067ddce9dd1839c613c43039fbd6785373ad
Nonso <nonsootoh@gmail.com>
2022-10-03 23:17:50 -0400

    chore: adds date validation on server

--- pages/api/reservation.ts
+++ pages/api/reservation.ts
@@ -17,9 +17,11 @@ export default async function handler(
     if (isDate(dateCheck)) {
       createReservation(req.body);
       res.status(200).json({ word: "success" });
+    } else {
+      res.status(404).json({ error: "Missing/invalid properties" });
     }
   } else {
-    res.status(403).json({
+    res.status(405).json({
       error:
         "This route only accepts post requests. Missing/invalid properties.",
     });
